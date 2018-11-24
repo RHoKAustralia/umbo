c25bd17566b8a003e2f23d212b73944092a6f23b
Ivan <ivan.rahn@gmail.com>
2018-11-24 16:52:15 +1100

    form, enums for gender

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,5 @@ class Patient < ApplicationRecord
   has_many :sessions
   has_many :patient_therapists
   has_many :therapists, through: :patient_therapists
+  enum gender: {'male': 0, 'female': 1}
 end
