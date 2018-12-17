90394700d00af1f16a916b1ff9843f2040da4d0c
David Armour <15181647+dav-armour@users.noreply.github.com>
2018-12-17 18:00:05 +1100

    add date select and time select pages add appointment confirm details page

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -19,7 +19,8 @@ class AppointmentsController < ApplicationController
   end
 
   def time_select
-    next_time = params['date'].to_time + 8.hours
+    @date = params['date']
+    next_time = @date.to_time + 8.hours
     @times2DArray = [[next_time]]
     10.times do
       next_time += 1.hour
@@ -28,16 +29,35 @@ class AppointmentsController < ApplicationController
     (1..3).each do |i|
       @times2DArray[i] = @times2DArray[i-1].map { |t| t + 15.minutes }
     end
+    next_time = @date.to_time + 8.hours
+    @timesArray = [next_time]
+    43.times do
+      next_time += 15.minutes
+      @timesArray << next_time
+    end
     @unavailable_times = [];
     # @unavailable_days = UnavailableDay.where(listing_id: @listing.id).pluck(:day)
     @therapist = Therapist.find(params[:therapist_id])
+    @appointment = Appointment.new
+    if params['start_time']
+      @appointment.start_time = params['start_time']
+    end
   end
 
   # GET /appointments/new
   def new
+    p params
+    start_time = parse_time_params(params[:appointment], :start_time)
+    @duration = params[:appointment][:duration].to_i
+    end_time = start_time + @duration.minutes
+    date = params[:appointment][:date].to_date
+    byebug
     @therapist = Therapist.find(params[:therapist_id])
-    @appointment = Appointment.new
-    params[:appointment] = Hash.new
+    @appointment = Appointment.new(
+      date: date,
+      start_time: start_time.strftime("%H:%M"),
+      end_time: end_time.strftime("%H:%M")
+    )
   end
 
   # GET /appointments/1/edit
@@ -47,15 +67,20 @@ class AppointmentsController < ApplicationController
   # POST /appointments
   # POST /appointments.json
   def create
+    p params
+    byebug
     @appointment = Appointment.new(appointment_params)
+    @appointment.therapist_id = params[:therapist_id]
+    @appointment.patient_id = current_user.patient.id
+    @appointment.total_cost = (@appointment.end_time - @appointment.start_time) / 3600 * 180
 
     respond_to do |format|
       if @appointment.save
-        format.html { redirect_to @appointment, notice: 'Appointment was successfully created.' }
-        format.json { render :show, status: :created, location: @appointment }
+        format.html { redirect_to root_path, notice: 'Appointment was successfully created.' }
+        # format.json { render :show, status: :created, location: @appointment }
       else
         format.html { render :new }
-        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+        # format.json { render json: @appointment.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -92,6 +117,20 @@ class AppointmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def appointment_params
-      params.require(:appointment).permit(:patient_id, :therapist_id, :date, :start_time, :end_time, :payment_date, :total_cost)
+      params.require(:appointment).permit(:date, :start_time, :end_time)
+    end
+
+    def parse_time_params params, label
+      begin
+        year   = params[(label.to_s + '(1i)').to_sym].to_i
+        month  = params[(label.to_s + '(2i)').to_sym].to_i
+        day   = params[(label.to_s + '(3i)').to_sym].to_i
+        hour   = (params[(label.to_s + '(4i)').to_sym] || 0).to_i
+        minute = (params[(label.to_s + '(5i)').to_sym] || 0).to_i
+
+        return Time.new(year,month,day,hour,minute)
+      rescue => e
+        return nil
+      end
     end
 end
